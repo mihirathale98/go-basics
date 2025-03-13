549ebbb0716cf908023fd2daacaf1ebfa87fef75
mihirathale98 <athale.m@northeastern.edu>
2025-03-13 07:45:48 -0400

    tutorial demo code 1-8

--- go_3.go
+++ go_3.go
@@ -6,10 +6,19 @@ func add(x float64, y float64) float64 {
 	return x+y 
 }
 
+func multiple(a, b string) (string, string) {
+	return a, b
+}
+
 func main() {
 	var num1, num2 float64 = 5.6, 9.5
 
 	fmt.Println(add(num1, num2))
+
+	w1, w2 := "Hey", "there"
+
+	
+	fmt.Println(multiple(w1, w2))
 }
 
 
